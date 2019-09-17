309cd91e2bd7afc104735a3feb2a8dd251376d4c
Marco <marco@new-host.station>
2019-09-17 20:14:29 +0200

    Basic PDB handling

--- coor.py
+++ coor.py
@@ -21,11 +21,64 @@ class XYZ:
         """Saving to XMol"""
         draft = open(path, "w")
         draft.write("{}\n\n".format(str(molecule.natoms)))
-        for idx, val in enumerate(molecule.element):
+        for idnx, val in enumerate(molecule.element):
             draft.write("{} \t{:.10f}\t {:.10f}\t {:.10f} \n".format(
                 val,
-                molecule.xyz[idx][0],
-                molecule.xyz[idx][1],
-                molecule.xyz[idx][2]
+                molecule.xyz[idnx][0],
+                molecule.xyz[idnx][1],
+                molecule.xyz[idnx][2]
                 ))
-        
+
+class PDB:
+    """
+    Loading PDB standard (Hierarchical Structure below)
+    CHAIN { RSN { ATOMS [ ATOM [] ] } }
+    ATOM = [ IDN, AT, AAT, CHAIN, RSN, X, Y, Z, ELEMENT ]
+    """
+    def __init__(self, path):
+        try:
+            with open(path) as file:
+                lines = file.readlines()
+            self.natoms = 0
+            self.prot = {}
+            atom = []
+            for line in lines:
+                if line.startswith("HETATM") or line.startswith("ATOM"):
+                    idn = line[6:12].strip()
+                    at = line[12:17].strip()
+                    aat = line[17:21].strip()
+                    chain = line[21:22].strip()
+                    rsn = line[22:27].strip()
+                    x_cart = line[30:39].strip()
+                    y_cart = line[38:47].strip()
+                    z_cart = line[46:55].strip()
+                    if line[76:79].strip() != "":
+                        element = line[76:79].strip()
+                    elif line[76:79].strip() == "":
+                        element = "H"
+                    atom = [
+                        idn,
+                        at,
+                        aat,
+                        chain,
+                        rsn,
+                        float(x_cart),
+                        float(y_cart),
+                        float(z_cart),
+                        element
+                    ]
+                    if chain not in self.prot:
+                        self.prot[chain] = {}
+                        self.prot[chain][rsn] = [atom]
+                    else:
+                        if rsn not in self.prot[chain]:
+                            self.prot[chain][rsn] = [atom]
+                        else:
+                            self.prot[chain][rsn].append(atom)
+                    self.natoms += 1
+            print("PDB succesfully loaded!\n")
+            print("Atoms...\t\t\t{}".format(str(self.natoms)))
+            print("Chains...\t\t\t{} ({})".format(len(self.prot), "/".join([*self.prot])))
+        except FileNotFoundError as detail:
+            print("\n{}".format(detail))
+            quit()
