f8c6109bfb74e4c733c91058dff48fea0b6cc16d
AresValley <aresvalley.info@gmail.com>
2019-09-18 09:08:32 +0200

    Solved a bug in utilites

--- utilities.py
+++ utilities.py
@@ -6,13 +6,18 @@ def dist(atom_a, atom_b):
     Calulate the distance between two atoms
     atom_a = [X, Y, Z]
     """
-    return np.linalg.norm(atom_a-atom_b)
+    atom_a = np.array(atom_a)
+    atom_b = np.array(atom_b)
+    return np.linalg.norm(atom_a - atom_b)
 
 def ang(atom_a, atom_b, atom_c):
     """
     Calulate the angle given three atoms
     atom_a = [X, Y, Z]
     """
+    atom_a = np.array(atom_a)
+    atom_b = np.array(atom_b)
+    atom_c = np.array(atom_c)
     b_a = atom_a - atom_b
     b_c = atom_c - atom_b
     rad_angle = np.arccos(
