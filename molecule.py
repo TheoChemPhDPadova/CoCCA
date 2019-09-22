8938a2ef50db7e5b7648649fa5b4dd4289bd6075
ppravatto <TheoChemPhDPadova@gmail.com>
2019-09-22 18:13:36 +0200

    Added IR spectrum plotter and few comments to the code

--- molecule.py
+++ molecule.py
@@ -1,19 +1,25 @@
 import coor
 import constants
+import numpy as np
+import matplotlib.pyplot as plt
 
 def molecule_compare(mol_1, mol_2, verbose=False):
     """
     Return True if the two molecules are equal and the .xyz files have the same ordering. 
     If a verbose output is required the flag "verbose=True" can be set
     """
+    #Check if the arguments are MOL class istances
     if isinstance(mol_1, MOL) == False or isinstance(mol_2, MOL) == False:
         print(""""ERROR: The function "molecule_compare" can handle only MOL class type objects""")
         quit()
+    #Check if the number of atoms in the two molecules are the same
     if mol_1.natoms != mol_2.natoms:
         if verbose==True: print("The two molecules have different number of atoms")
         return False
+    #Check if the composition of the two molecule is the same
     if mol_1.composition() == mol_2.composition():
         if verbose==True: print("The two molecules have the same composition")
+        #Check if the ordering of the atoms in the xyz file is the same
         if mol_1.element == mol_2.element:
             if verbose==True: print("The ordering of the two .xyz files is the same")
             return True
@@ -28,19 +34,23 @@ def rigid_linear_transit(mol_start, mol_end, steps, filename):
     "mol_start" to the end one "mol_end" with a number of step set by the variable "steps".
     The generated files have the name "root_step.xyz" in which root is set by the varibale "filename"
     """
+    #Check if the molecules are isomers of the same molecule and if the atom ordering is the same
     if molecule_compare(mol_start, mol_end) == False:
         print("ERROR: The starting and final molecules passed to the rigid linear transit routine are different")
         quit()
+    #Compute for each atom the distance between the start and end structure
     total_displacement = []
     for atom in range(0, mol_start.natoms):
         my_list = []
         for coord in range(0, 3):
             my_list.append(mol_end.xyz[atom][coord] - mol_start.xyz[atom][coord])
         total_displacement.append(my_list)
+    #Iterate over each step of the linear transit
     for step in range(1, steps+1):
         out_file = open(filename + "_" + str(step) + ".xyz", 'w')
         out_file.write(str(mol_start.natoms) + "\n")
         out_file.write('\n')
+        #Compute the new position of each atom in the file
         for atom in range(0, mol_start.natoms):
             out_file.write(mol_start.element[atom])
             for coord in range(0, 3):
@@ -48,6 +58,84 @@ def rigid_linear_transit(mol_start, mol_end, steps, filename):
                 out_file.write('\t' + str(new_coord))
             out_file.write('\n')
         out_file.close()
+
+
+def plot_ir_spectrum(molecule, *args, style="bar", resolution=0.1, path="", show=True):
+    """
+    Plot the infrared spectrum of the molecule starting from the .hess file data. The molecule
+    is passed via a MOL class object. The style of plot is set with the flag style: type="bar"
+    simply return a stem plot of the intensities, style="gaussian" set the plot of the absorption
+    profile with gaussian broadening, style="lorentzian" set the plot of the absorption profile
+    with lorentzian broadening. If "gaussian" is selected the sigma of the gaussian in cm^-1 must
+    be passed as an additiona argument. The same is true in the case of "lorentzian" in which the
+    linewidth in cm^-1 must be passed as an additiona argument. The resolution of the plot is set,
+    in cm^-1 units, by the flag resolution. By default the resolution in 0.1cm^-1. The flag 
+    path="" allow the selection of the directory in which the spectrum must be saved, if nothing 
+    is set as path no file will be saved. The flag show allow to activate (True) or deactivate 
+    (False) the graphical output.
+    """
+    #Define the Gaussian and Lorentziam function of unitary height for x=x0
+    def unitary_height_gaussian(x, x0, sigma):
+        return np.exp(-(x-x0)**2/(2*sigma**2))
+    def unitary_height_lorentzian(x, x0, gamma):
+        return  (gamma/2)**2 /((x - x0)**2 + (gamma/2)**2)
+    #Verify that, if needed, the width parameter is passed as the only additional value
+    if len(args) > 1:
+        print("""ERROR: Too many arguments passed to the function "plot_ir_spectrum" """)
+    elif len(args) == 1:
+        width = float(args[0])
+    elif len(args) == 0 and style != "bar":
+        print("""ERROR: Missing width parameter in function "plot_ir_spectrum" """)
+    if isinstance(molecule, MOL) == False:
+        print("""ERROR: The function "plot_ir_spectrum" can handle only MOL class type objects""")
+        quit()
+    #Extract the data of frequency and intensity for each band neglecting the zero frequency modes
+    vibr_freq = []
+    trans_int = []
+    if hasattr(molecule, 'ir_spectrum'):
+        f_list = [i[0] for i in molecule.ir_spectrum]
+        i_list = [i[1] for i in molecule.ir_spectrum]
+        for i in range(0, len(f_list)):
+            if f_list[i] != 0:
+                vibr_freq.append(f_list[i])
+                trans_int.append(i_list[i])
+    else:
+        print("ERROR: The object " + str(molecule) + " has no IR spectrum data loaded")
+        quit()
+    #Initialize a figure object
+    fig, ax = plt.subplots(figsize=(18, 7))
+    #Check the type of plot selected
+    if style == "bar":
+        ax.stem(vibr_freq, trans_int)
+    elif style == "gaussian" or style == "lorentzian":
+        frequency_axis = []
+        spectrum = []
+        #Generate the plot with the selected resolution keeping 200 cm^-1 at each side
+        f = min(vibr_freq) - 200
+        while f<= max(vibr_freq) + 200:
+            frequency_axis.append(f)
+            point = 0
+            for index, center in enumerate(vibr_freq):
+                if style == "gaussian":
+                    point += trans_int[index]*unitary_height_gaussian(f, center, width)
+                if style == "lorentzian":
+                    point += trans_int[index]*unitary_height_lorentzian(f, center, width)
+            spectrum.append(point)
+            f += float(resolution)
+        ax.plot(frequency_axis, spectrum, color='red')
+    else:
+        print("ERROR: The style option " + str(style) + " is not valid" )
+    ax.set_xlabel(r"Wavenumber [$cm^{-1}$]")
+    ax.set_ylabel(r"Intensity [$km/mol$]")
+    ax.grid(b=True, which='major', color='#C0C0C0')
+    ax.grid(b=True, which='minor', color='#E0E0E0')
+    plt.tight_layout()
+    #Save the file if needed
+    if path != "":
+        plt.savefig(path, dpi=600)
+    #Show the plot if the option is selected
+    if show == True:
+        plt.show()
 
 
 class MOL(coor.XYZ):
@@ -83,15 +171,19 @@ class MOL(coor.XYZ):
         The list can be ordered as %mass abbundance (default) or by number of atoms selecting order="number"
         """
         comp = []
+        #Create a list of atoms divided by element with the relative number of them in the molecule
         for atom in self.element:
             known_atoms = [i[0] for i in comp] 
             if atom in known_atoms:
                 comp[known_atoms.index(atom)][1] += 1
             else:
                 comp.append([atom, 1, 0])
+        #Complete the last column with the percentual in mass of each element
         for atom in comp:
             atom[2] = 100.*atom[1]*constants.atomic_mass[atom[0]]/self.mass()
+        #Order the elements by abbundance in mass
         comp.sort(key=lambda x: x[2], reverse=True)
+        #If selected order the elements by number
         if order == "number":
             comp.sort(key=lambda x: x[1], reverse=True) 
         return comp
@@ -101,6 +193,7 @@ class MOL(coor.XYZ):
         The function loads into the MOL class object the data conteined into the .hess file
         specified in path. For a verbose output about errors use the flag "verbose=True"
         """
+        #Define a function to load a matrix from an orca hessian file
         def load_orca_hesstype_mat(datalist, header_position):
             matrix = []
             dim = [int(i) for i in datalist[header_position + 1].split()]
@@ -122,19 +215,23 @@ class MOL(coor.XYZ):
             return matrix
 
         try:
+            #Open the file stripping the newline escape code
             with open(path) as file:
                 lines = file.readlines()
                 lines = [line.strip('\n') for line in lines]
+            #Search and load the hessian
             try:
                 hessian_index = lines.index("$hessian")
                 self.hessian_matrix = load_orca_hesstype_mat(lines, hessian_index)
             except ValueError:
                 if verbose == True: print("WARNING: no hessian matrix found")
+            #Search and load the normal mode matrix
             try:
                 norm_modes_index = lines.index("$normal_modes")
                 self.normal_modes_matrix = load_orca_hesstype_mat(lines, norm_modes_index)
             except ValueError:
                 if verbose == True: print("WARNING: no normal modes matrix found")
+            #Search and load the frequencies
             try:
                 frequencies_index = lines.index("$vibrational_frequencies")
                 self.vibr_frequencies = []
@@ -144,7 +241,16 @@ class MOL(coor.XYZ):
                     self.vibr_frequencies.append(float(row[1]))
             except ValueError:
                 if verbose == True: print("WARNING: no vibrational frequencies list found")
+            #Search and load the vibrational spectrum
+            try:
+                ir_spectrum_index = lines.index("$ir_spectrum")
+                dim = int(lines[frequencies_index + 1])
+                self.ir_spectrum = []
+                for index in range(0, dim):
+                    row = lines[ir_spectrum_index + 2 + index].split()
+                    self.ir_spectrum.append([float(i) for i in row])
+            except ValueError:
+                if verbose == True: print("WARNING: no infrared spectrum matrix found")
         except FileNotFoundError as detail:
             print("\n{}".format(detail))
             quit()
-
