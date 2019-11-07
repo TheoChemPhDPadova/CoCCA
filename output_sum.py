7bcee9d89a7cd15a7db1e914714ac4bcdfa82195
Marco <marcodallatiezza@gmail.com>
2019-11-07 14:53:16 +0100

    Complete refactoring of output_sum source code

--- output_sum.py
+++ output_sum.py
@@ -1,244 +1,110 @@
-"""TO-BE-FIXED"""
+"""QM output reader"""
+import argparse, readline, glob, QM_parser
 import matplotlib.pyplot as plt
-import numpy as np
-import fileinput
+
+def complete(text, state):
+    return (glob.glob(text+'*')+[None])[state]
+
+readline.set_completer_delims(' \t\n;')
+readline.parse_and_bind("tab: complete")
+readline.set_completer(complete)
+
+PARSER = argparse.ArgumentParser()
+PARSER.add_argument("-i", "--input", type=str, help="Path of the output file...")
+ARGS = PARSER.parse_args()
+
+if ARGS.input:
+    FILENAME = ARGS.input
+else:
+    FILENAME = input("Enter output file path...\t\t")
+
+with open(FILENAME) as file:
+    out = file.read()
+
+if 'Amsterdam Density Functional  (ADF)' in out:
+    src = QM_parser.ADF(FILENAME)
+elif 'O   R   C   A' in out:
+    src = QM_parser.ORCA(FILENAME)
+else:
+    print('\nSorry, output file not supported!')
+    quit()
 
 plt.rc('font', family='serif')
+fig = plt.figure() 
+fig.canvas.set_window_title(src.version)
 
-tmp=[]
-E=[]
-EC=[]
-EClim=[]
-Rg=[]
-Rglim=[]
-Mg=[]
-Mglim=[]
-Rs=[]
-Rslim=[]
-Ms=[]
-Mslim=[]
-eigen=0
-counter=1
-
-Eigen1=[]
-Eigen2=[]
-Eigen3=[]
-Eigen4=[]
-Eigen5=[]
-
-for line in fileinput.input():
-	if "Geometry Convergence after Step" in line:
-		version="ADFnew"
-		break
-	if " Geometry Convergence Tests" in line:
-		version="ADFold"
-		break
-	if "FINAL SINGLE POINT ENERGY" in line:
-		version="ORCA"
-		break
-	if "Predicted change in Energy" in line:
-		version="G16"
-		break
-
-fileinput.close()
-
-if version=="ADFnew":
-	for line in fileinput.input():
-		if "current energy" in line:
-			tmp=line.split()
-			E.append(float(tmp[2]))
-		if "energy change" in line:
-			tmp=line.split()
-			EC.append(asb(float(tmp[2])))
-			EClim.append(float(tmp[3]))
-		if "constrained gradient rms" in line:
-			tmp=line.split()
-			Rg.append(float(tmp[3]))
-			Rglim.append(float(tmp[4]))
-		if "constrained gradient max" in line:
-			tmp=line.split()
-			Mg.append(float(tmp[3]))
-			Mglim.append(float(tmp[4]))
-		if "cart. step rms" in line:
-			tmp=line.split()
-			Rs.append(float(tmp[3]))
-			Rslim.append(float(tmp[4]))
-		if "cart. step max" in line:
-			tmp=line.split()
-			Ms.append(float(tmp[3]))
-			Mslim.append(float(tmp[4]))
-	fig = plt.figure() 
-	fig.canvas.set_window_title('ADF (New Version)')
-	EC[0]=None
-
-if version=="ADFold":
-	for line in fileinput.input():
-		if "         new :" in line:
-			tmp=line.split()
-			E.append(float(tmp[2]))
-		if "change in energy" in line:
-			tmp=line.split()
-			EC.append(abs(float(tmp[3])))
-			EClim.append(float(tmp[4]))
-		if "gradient rms" in line:
-			tmp=line.split()
-			Rg.append(float(tmp[2]))
-			Rglim.append(float(tmp[3]))
-		if "gradient max" in line:
-			tmp=line.split()
-			Mg.append(float(tmp[2]))
-			Mglim.append(float(tmp[3]))
-		if "cart. step rms" in line:
-			tmp=line.split()
-			Rs.append(float(tmp[3]))
-			Rslim.append(float(tmp[4]))
-		if "cart. step max" in line:
-			tmp=line.split()
-			Ms.append(float(tmp[3]))
-			Mslim.append(float(tmp[4]))
-	fig = plt.figure() 
-	fig.canvas.set_window_title('ADF (Old Version)')
-	EC[0]=None
-	
-if version=="ORCA":
-	for line in fileinput.input():
-		if "FINAL SINGLE POINT ENERGY" in line:
-			tmp=line.split()
-			E.append(float(tmp[4]))
-		if "          Energy change" in line:
-			tmp=line.split()
-			EC.append(abs(float(tmp[2])))
-			EClim.append(float(tmp[3]))
-		if "          RMS gradient" in line:
-			tmp=line.split()
-			Rg.append(float(tmp[2]))
-			Rglim.append(float(tmp[3]))
-		if "          MAX gradient" in line:
-			tmp=line.split()
-			Mg.append(float(tmp[2]))
-			Mglim.append(float(tmp[3]))
-		if "          RMS step" in line:
-			tmp=line.split()
-			Rs.append(float(tmp[2]))
-			Rslim.append(float(tmp[3]))
-		if "          MAX step" in line:
-			tmp=line.split()
-			Ms.append(float(tmp[2]))
-			Mslim.append(float(tmp[3]))
-		if eigen==1:
-			tmp=line.split()
-			Eigen1.append(float(tmp[0]))
-			Eigen2.append(float(tmp[1]))
-			Eigen3.append(float(tmp[2]))
-			Eigen4.append(float(tmp[3]))
-			Eigen5.append(float(tmp[4]))
-			print(str(counter) + '   ' + str(line.strip()))
-			counter=counter+1
-			eigen=0
-		if "Lowest eigenvalues" in line:
-			eigen=1
-
-
-	fig = plt.figure() 
-	fig.canvas.set_window_title('ORCA')
-	EC.insert(0, None)
-	EClim.insert(0, EClim[1])
-	del E[-1]
-
-if version=="G16":
-	for line in fileinput.input():
-		if "SCF Done:" in line:
-			tmp=line.split()
-			E.append(float(tmp[4]))
-		if "RMS     Force" in line:
-			tmp=line.split()
-			Rg.append(float(tmp[2]))
-			Rglim.append(float(tmp[3]))
-		if "Maximum Force" in line:
-			tmp=line.split()
-			Mg.append(float(tmp[2]))
-			Mglim.append(float(tmp[3]))
-		if "RMS     Displacement" in line:
-			tmp=line.split()
-			Rs.append(float(tmp[2]))
-			Rslim.append(float(tmp[3]))
-		if "Maximum Displacement" in line:
-			tmp=line.split()
-			Ms.append(float(tmp[2]))
-			Mslim.append(float(tmp[3]))
-		if "Eigenvalues ---   -" in line:
-			tmp=line.split()
-			Eigen1.append(float(tmp[2]))
-			Eigen2.append(float(tmp[3]))
-			Eigen3.append(float(tmp[4]))
-			Eigen4.append(float(tmp[5]))
-			Eigen5.append(float(tmp[6]))
-			print(str(counter) + ':   ' + line.strip())
-			counter=counter+1
-	fig = plt.figure() 
-	fig.canvas.set_window_title('Gaussian 16 (G16)')
-	for idx, val in enumerate(E):
-		EC.append(abs(val-E[idx-1]))
-	EC[0]=None
-
-ax1=plt.subplot(331)
-plt.plot (E, marker=".", markersize=7, lw=1)
+ax1 = plt.subplot(331)
+plt.plot (src.ene, marker=".", markersize=7, lw=1)
 plt.ylabel('Hartree')
-plt.title("Energy")
-plt.xlim(0, len(E)-1)
+plt.title('Energy')
+plt.xlim(0, len(src.ene)-1)
 
 plt.subplot(332)
-plt.plot(EC, marker=".", markersize=7, lw=1)
-plt.plot (EClim,'r--')
+plt.plot (src.grdmax, marker=".", markersize=7, lw=1)
+plt.axhline(y=src.grdmaxlim, color='r', linestyle='-', lw=1.5)
+if src.grdmax[-1] < src.grdmaxlim:
+    plt.axhline(y=src.grdmaxlim, color='#3bd636', linestyle='-', lw=1.5)
+else:
+    plt.axhline(y=src.grdmaxlim, color='r', linestyle='-', lw=1.5)
 plt.ylabel('Hartree')
-plt.title("|Energy Change|")
-plt.xlim(0, len(E)-1)
+plt.title('Gradient MAX')
+plt.xlim(0, len(src.ene)-1)
 
 plt.subplot(333)
-plt.plot (Rg, marker=".", markersize=7, lw=1)
-plt.plot (Rglim,'r--')
-plt.ylabel('Hartree')
-if version=="G16":
-	plt.title("RMS Force")
+plt.plot (src.stpmax, marker=".", markersize=7, lw=1)
+plt.axhline(y=src.stpmaxlim, color='r', linestyle='-', lw=1.5)
+if src.stpmax[-1] < src.stpmaxlim:
+    plt.axhline(y=src.stpmaxlim, color='#3bd636', linestyle='-', lw=1.5)
 else:
-	plt.title("RMS gradient")
-plt.xlim(0, len(E)-1)
+    plt.axhline(y=src.stpmaxlim, color='r', linestyle='-', lw=1.5)
+plt.ylabel('Hartree')
+plt.title('Displacement MAX')
+plt.xlim(0, len(src.ene)-1)
 
 plt.subplot(334)
-plt.plot (Mg, marker=".", markersize=7, lw=1)
-plt.plot (Mglim,'r--')
-plt.ylabel('Hartree')
-if version=="G16":
-	plt.title("MAX Force")
+plt.plot(src.enechg, marker=".", markersize=7, lw=1)
+plt.axhline(y=src.enelim, color='r', linestyle='-', lw=1.5)
+if src.enechg[-1] < src.enelim:
+    plt.axhline(y=src.enelim, color='#3bd636', linestyle='-', lw=1.5)
 else:
-	plt.title("MAX gradient")
-plt.xlim(0, len(E)-1)
+    plt.axhline(y=src.enelim, color='r', linestyle='-', lw=1.5)
+plt.ylabel('Hartree')
+plt.title('|Energy Change|')
+plt.xlim(0, len(src.ene)-1)
 
 plt.subplot(335)
-plt.plot (Rs, marker=".", markersize=7, lw=1)
-plt.plot (Rslim,'r--')
+plt.plot(src.grdrms, marker=".", markersize=7, lw=1)
+plt.axhline(y=src.grdrmslim, color='r', linestyle='-', lw=1.5)
+if src.grdrms[-1] < src.grdrmslim:
+    plt.axhline(y=src.grdrmslim, color='#3bd636', linestyle='-', lw=1.5)
+else:
+    plt.axhline(y=src.grdrmslim, color='r', linestyle='-', lw=1.5)
 plt.ylabel('Hartree')
-plt.title("RMS step displacement")
-plt.xlim(0, len(E)-1)
+plt.title('Gradient RMS')
+plt.xlim(0, len(src.ene)-1)
 
 plt.subplot(336)
-plt.plot (Ms, marker=".", markersize=7, lw=1)
-plt.plot (Mslim,'r--')
+plt.plot (src.stprms, marker=".", markersize=7, lw=1)
+plt.axhline(y=src.stprmslim, color='r', linestyle='-', lw=1.5)
+if src.stprms[-1] < src.stprmslim:
+    plt.axhline(y=src.stprmslim, color='#3bd636', linestyle='-', lw=1.5)
+else:
+    plt.axhline(y=src.stprmslim, color='r', linestyle='-', lw=1.5)
 plt.ylabel('Hartree')
-plt.title("MAX step displacement")
-plt.xlim(0, len(E)-1)
+plt.title('Displacement RMS')
+plt.xlim(0, len(src.ene)-1)
 
 plt.subplot(337)
-plt.plot (Eigen1, marker=".", markersize=7, lw=2)
-plt.plot (Eigen2, marker=".", markersize=7, lw=1)
-plt.plot (Eigen3, marker=".", markersize=7, lw=1)
-plt.plot (Eigen4, marker=".", markersize=7, lw=1)
-plt.plot (Eigen5, marker=".", markersize=7, lw=1)
-plt.title("Hessian Eigenvector")
-plt.xlim(0, len(E)-1)
+plt.plot(src.eigen[0], marker=".", markersize=7, lw=1)
+plt.plot(src.eigen[1], marker=".", markersize=7, lw=1)
+plt.plot(src.eigen[2], marker=".", markersize=7, lw=1)
+plt.plot(src.eigen[3], marker=".", markersize=7, lw=1)
+plt.plot(src.eigen[4], marker=".", markersize=7, lw=1)
 
+plt.axhline(y=0, color='black', linestyle='--', lw=1)
+plt.title('Hessian Eigenvector')
+plt.xlim(0, len(src.ene)-1)
 
-#plt.tight_layout()
 mng = plt.get_current_fig_manager()
 mng.full_screen_toggle()
 plt.show()
