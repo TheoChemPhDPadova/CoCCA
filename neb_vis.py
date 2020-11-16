64becb5ba484627104bdac78669918835432f50b
AresValley <aresvalley.info@gmail.com>
2020-11-16 13:57:40 +0100

    NEB_visualizer transition to Plotly

--- neb_vis.py
+++ neb_vis.py
@@ -1,18 +1,14 @@
 """Neb Visualizer"""
 import sys, os
-import argparse, utils
-import matplotlib.pyplot as plt
+import utils
+import plotly.graph_objects as go
 
 
-def main(ARGS):
+def main(path):
     utils.TITLE("NEB Visualizer (ORCA)")
+    fig = go.Figure()
 
-    if ARGS.input:
-        FILENAME = ARGS.input
-    else:
-        FILENAME = input("Enter .interp file path...\t\t")
-
-    with open(FILENAME) as file:
+    with open(path) as file:
         lines = file.readlines()
 
     idx_pts = []
@@ -33,42 +29,47 @@ def main(ARGS):
 
     for i in idx_pts:
         if i == idx_pts[0]:
-            x = [float(lines[j].split()[0]) for j in range(i, i+pts_n)]
-            y = [float(lines[j].split()[2])*utils.ha2kcal for j in range(i, i+pts_n)]
-            plt.scatter(x, y, s=15, c="orange")
+            x = [float(lines[j].split()[0]) for j in range(i, i + pts_n)]
+            y = [float(lines[j].split()[1]) * utils.ha2kcal for j in range(i, i + pts_n)]
+            fig.add_trace(go.Scatter(x=x, y=y, mode='markers', name='markers', marker=dict(color='darkorange')))
         elif i == idx_pts[-1]:
-            x = [float(lines[j].split()[0]) for j in range(i, i+pts_n)]
-            y = [float(lines[j].split()[2])*utils.ha2kcal for j in range(i, i+pts_n)]
-            plt.scatter(x, y, s=15, c="cornflowerblue", zorder=10)
+            x = [float(lines[j].split()[0]) for j in range(i, i + pts_n)]
+            y = [float(lines[j].split()[1]) * utils.ha2kcal for j in range(i, i + pts_n)]
+            fig.add_trace(go.Scatter(x=x, y=y, mode='markers', name='markers', marker=dict(color='cornflowerblue')))
         else:
-            x = [float(lines[j].split()[0]) for j in range(i, i+pts_n)]
-            y = [float(lines[j].split()[2])*utils.ha2kcal for j in range(i, i+pts_n)]
-            plt.scatter(x, y, s=7, c="gainsboro")
+            x = [float(lines[j].split()[0]) for j in range(i, i + pts_n)]
+            y = [float(lines[j].split()[1]) * utils.ha2kcal for j in range(i, i + pts_n)]
+            fig.add_trace(go.Scatter(x=x, y=y, mode='markers', name='markers', marker=dict(color='gainsboro')))
 
     for i in idx_int:
         if i == idx_int[0]:
-            x = [float(lines[j].split()[0]) for j in range(i, i+int_n)]
-            y = [float(lines[j].split()[2])*utils.ha2kcal for j in range(i, i+int_n)]
-            plt.plot(x, y, c="orange")
+            x = [float(lines[j].split()[0]) for j in range(i, i + int_n)]
+            y = [float(lines[j].split()[1]) * utils.ha2kcal for j in range(i, i + int_n)]
+            fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='lines', line=dict(color='darkorange')))
         elif i == idx_int[-1]:
-            x = [float(lines[j].split()[0]) for j in range(i, i+int_n)]
-            y = [float(lines[j].split()[2])*utils.ha2kcal for j in range(i, i+int_n)]
-            plt.plot(x, y, c="cornflowerblue", zorder=10)
+            x = [float(lines[j].split()[0]) for j in range(i, i + int_n)]
+            y = [float(lines[j].split()[1]) * utils.ha2kcal for j in range(i, i + int_n)]
+            fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='lines', line=dict(color='cornflowerblue')))
         else:
-            x = [float(lines[j].split()[0]) for j in range(i, i+int_n)]
-            y = [float(lines[j].split()[2])*utils.ha2kcal for j in range(i, i+int_n)]
-            plt.plot(x, y, c="gainsboro")
+            x = [float(lines[j].split()[0]) for j in range(i, i + int_n)]
+            y = [float(lines[j].split()[1]) * utils.ha2kcal for j in range(i, i + int_n)]
+            fig.add_trace(go.Scatter(x=x, y=y, mode='lines', name='lines', line=dict(color='gainsboro')))
 
-    plt.xlim(0, 1)
-    plt.show()
+    fig.update_layout(
+        title="ORCA Nudged Elastic Band ",
+        xaxis_title="Iteration",
+        yaxis_title="E / kcal mol<sup>-1</sup>",
+        template="plotly_white",
+        showlegend=False
+    )
+    fig.show()
+    utils.NT()
 
 
 if __name__ == "__main__":
     try:
-        PARSER = argparse.ArgumentParser()
-        PARSER.add_argument("-i", "--input", type=str, help="Path of the .interp file (ORCA)")
-        ARGS = PARSER.parse_args()
-        main(ARGS)
+        path = input("Enter .interp file path...\t\t")
+        main(path)
     except KeyboardInterrupt:
         print("Interrupted by user")
         try:
